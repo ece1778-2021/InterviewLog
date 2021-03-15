6bcb8af52b825f31f4310d997a35cc32eaa3b12f
careal305 <61394858+careal305@users.noreply.github.com>
2021-03-15 14:31:34 -0400

    add model container

---
+++ app/src/main/java/com/example/interviewlogapp/tagList.java
@@ -0,0 +1,22 @@
+package com.example.interviewlogapp;
+
+import android.util.Log;
+
+public class tagList {
+    public String tag;
+
+    public tagList(){
+    }
+
+    public tagList(String tag) {
+        this.tag = tag;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
